530c4e092787369a5afd693dabe21cc5323f8791
SukbeomH <brent93.dev@gmail.com>
2024-07-09 11:41:37 +0900

    add escape

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -69,7 +69,7 @@ const parser = new Parser({
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
         // text += `#### <a href=${link}>${title}</a></br>`;
-        text += `#### ![📝](${link}) [${title}](${link})</br>`;
+        text += `#### ![📝](${link}) [${title}](${link})</br>\ `;
     }
     // 최종적으로 블로그 게시물 목록을 닫음
     text += '\
