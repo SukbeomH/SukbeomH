57fca64af007067b1db7bf1297689f588644c86e
SukbeomH <brent93.dev@gmail.com>
2024-07-09 11:33:24 +0900

    style change

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -62,12 +62,12 @@ const parser = new Parser({
     const feed = await parser.parseURL('https://veritasgarage.tistory.com/rss');
 
     // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 15; i++) {
         const {title, link} = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
-        text += `<a href=${link}>${title}</a></br>`;
+        text += `<a href=${link}>#### ${title}</a></br>`;
     }
     // 최종적으로 블로그 게시물 목록을 닫음
     text += '</div>';
